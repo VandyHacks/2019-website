94cad30e333e75ecc0a36f5f392287906de35ca4
Benjamin Cooper <bencooper222@gmail.com>
2019-09-04 17:50:14 -0500

    Add Gatsby React SVG

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,9 +28,13 @@ module.exports = {
       },
     },
     'gatsby-plugin-styled-components',
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
   ],
 };
