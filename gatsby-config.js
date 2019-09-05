422562b1c055eeeee25c18879ff26b6a7d6d45b0
Benjamin Cooper <bencooper222@gmail.com>
2019-09-04 20:16:01 -0500

    Add process.env title detection

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `VandyHacks VI`,
+    title: process.env.STATUS == null ? `VandyHacks VI` : `${process.env.STATUS} VandyHacks VI`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `The VandyHacks Team`,
   },
