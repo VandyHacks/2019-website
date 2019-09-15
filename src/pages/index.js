eb4c5216cb81cafea1251e39d6ce89e961cac719
xuezzou <ilfam1999@gmail.com>
2019-09-15 16:33:28 -0500

    add styled component?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import SEO from '../components/seo';
 
@@ -7,9 +8,19 @@ import Welcome from '../components/welcome';
 import FAQ from '../components/faq';
 import Schedule from '../components/schedule';
 import SponsorBox from '../components/sponsorBox';
+import Try from '../images/bigLogo.svg';
+
+const Container = styled.div`
+	width: 50%;
+    text-align: center;
+    margin: 3em auto';
+`;
 
 const IndexPage = () => (
 	<div>
+		<Container>
+			<Try />
+		</Container>
 		<SEO title="" />
 		{
 			// insert VH logo
