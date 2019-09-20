c39306ae0e92e131778f7c25df0f9fffd295fb91
xuezzou <ilfam1999@gmail.com>
2019-09-20 16:28:37 -0500

    update mobile version of welcome and faq page

--- src/components/faq/column.js
+++ src/components/faq/column.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import QuestionSet from './questionSet';
+import useWindowWidth from '../utils/useWindowWidth';
 
 // accepted props of a FAQ column (2 on desktop, 1 on mobile + tablet)
 
@@ -11,13 +12,34 @@ const ColumnContainer = styled.div`
 	width: 45%;
 `;
 
+const ColumnContainerMobile = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+`;
+
 const Column = ({ questions }) => {
 	const [openedIndex, setOpened] = useState(0);
+	const isMobile = useWindowWidth() <= 768;
 
 	const onQuestionClicked = index => {
 		setOpened(openedIndex === index ? -1 : index);
 	};
-
+	if (isMobile) {
+		return (
+			<ColumnContainerMobile>
+				{questions.map(({ question, answer }, i) => (
+					<QuestionSet
+						key={question}
+						question={question}
+						answer={answer}
+						shouldOpen={openedIndex === i}
+						onClick={() => onQuestionClicked(i)}
+					/>
+				))}
+			</ColumnContainerMobile>
+		);
+	}
 	return (
 		<ColumnContainer>
 			{questions.map(({ question, answer }, i) => (
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 import InViewMonitor from 'react-inview-monitor';
+import useWindowWidth from './utils/useWindowWidth';
 
 import WelcomeTitle from '../images/welcomeText.svg';
 import WelcomeBorder from '../images/welcomeBorder.svg';
 import Eraser from '../images/stationeryEraser.svg';
 import Pen2 from '../images/stationeryPen2.svg';
 import Pen3 from '../images/stationeryPen3.svg';
+import LowerBar from '../images/faqLowerBar.svg';
 
 const Container = styled.div`
 	width: 100%;
@@ -17,6 +19,11 @@ const Container = styled.div`
 
 const WelcomeLogoStyle = styled.div`
 	width: 18em;
+	justify-content: left;
+`;
+
+const WelcomeLogoMobileStyle = styled.div`
+	width: 13em;
 	justify-content: left;
 `;
 
@@ -33,6 +40,17 @@ const WelcomeTextStyle = styled.p`
 	margin-left: 7em;
 `;
 
+const WelcomeTextMobileStyle = styled.p`
+	position: relative;
+	margin-left: 0.3em;
+	margin-right: 1em;
+	text-align: left;
+	font-size: 1em;
+	display: block;
+	line-height: 1.5em;
+	width: 17em;
+`;
+
 const WelcomeBoxStyle = styled.div`
 	position: relative;
 	width: 33em;
@@ -41,12 +59,28 @@ const WelcomeBoxStyle = styled.div`
 	margin-bottom: 5em;
 `;
 
+const WelcomeBoxMobileStyle = styled.div`
+	position: relative;
+	width: 100%;
+	opacity: 1;
+	margin: 3em auto;
+`;
+
 const EraserStyle = styled.div`
 	position: absolute;
 	top: 17em;
 	left: 94%;
 	width: 5em;
 	opacity: 1;
+`;
+
+const EraserMobileStyle = styled.div`
+	position: absolute;
+	top: -5em;
+	left: 50%;
+	width: 2.5em;
+	opacity: 1;
+	transform: rotate(-80deg);
 `;
 
 const Pen2Style = styled.div`
@@ -65,42 +99,87 @@ const Pen3Style = styled.div`
 	opacity: 1;
 `;
 
+const Pen3MobileStyle = styled.div`
+	position: absolute;
+	top: -0.5em;
+	left: 87%;
+	width: 3em;
+	opacity: 1;
+`;
+
+const LowerBarStyle = styled.div`
+	position: relative;
+	left: -1em;
+`;
+
 // place holder for text
 const welcomeText =
 	'VandyHacks is back this Fall in the Music City–and it’s ready to redesign hackathons.';
-const welcomeBoldPresent = 
-	'Presenting VandyHacks VI: Art Edition.'
+const welcomeBoldPresent = 'Presenting VandyHacks VI: Art Edition.';
 const welcomeTextCont =
 	'From November 1-3, hundreds of talented and creative students from across the nation will come together to learn, collaborate, and create exciting projects with the ambition to';
-const welcomeBoldCode = 
-	' Code a Masterpiece.'
+const welcomeBoldCode = ' Code a Masterpiece.';
 const Welcome = () => {
-	return (
-		<Container>
-			<WelcomeLogoStyle>
-				<WelcomeTitle />
-			</WelcomeLogoStyle>
+	const isMobile = useWindowWidth() <= 768;
+	if (isMobile) {
+		return (
+			<Container>
+				<WelcomeLogoMobileStyle>
+					<WelcomeTitle />
+				</WelcomeLogoMobileStyle>
 
-			<WelcomeBoxStyle>
-				<WelcomeBorder />
-				<WelcomeTextStyle>{welcomeText}</WelcomeTextStyle>
-				<WelcomeTextStyle style={{fontWeight:"bold", lineHeight:'0'}}>{welcomeBoldPresent}</WelcomeTextStyle>
-				<WelcomeTextStyle>{welcomeTextCont}</WelcomeTextStyle>
-					<WelcomeTextStyle style={{fontWeight:"bold", lineHeight:'0'}}>{welcomeBoldCode}</WelcomeTextStyle>
-			</WelcomeBoxStyle>
+				<WelcomeBoxMobileStyle>
+					<WelcomeTextMobileStyle>{welcomeText}</WelcomeTextMobileStyle>
+					<WelcomeTextMobileStyle style={{ fontWeight: 'bold', lineHeight: '0' }}>
+						{welcomeBoldPresent}
+					</WelcomeTextMobileStyle>
+					<WelcomeTextMobileStyle>{welcomeTextCont}</WelcomeTextMobileStyle>
+					<WelcomeTextMobileStyle style={{ fontWeight: 'bold', lineHeight: '0' }}>
+						{welcomeBoldCode}
+					</WelcomeTextMobileStyle>
 
-			<EraserStyle>
-				<Eraser />
-			</EraserStyle>
-			<Pen2Style>
-				<Pen2 />
-			</Pen2Style>
+					<LowerBarStyle>
+						<LowerBar />
+					</LowerBarStyle>
+				</WelcomeBoxMobileStyle>
 
-			<Pen3Style>
-				<Pen3 />
-			</Pen3Style>
-		</Container>
-	);
+				<Pen3MobileStyle>
+					<Pen3 />
+				</Pen3MobileStyle>
+			</Container>
+		);
+	} else {
+		return (
+			<Container>
+				<WelcomeLogoStyle>
+					<WelcomeTitle />
+				</WelcomeLogoStyle>
+
+				<WelcomeBoxStyle>
+					<WelcomeBorder />
+					<WelcomeTextStyle>{welcomeText}</WelcomeTextStyle>
+					<WelcomeTextStyle style={{ fontWeight: 'bold', lineHeight: '0' }}>
+						{welcomeBoldPresent}
+					</WelcomeTextStyle>
+					<WelcomeTextStyle>{welcomeTextCont}</WelcomeTextStyle>
+					<WelcomeTextStyle style={{ fontWeight: 'bold', lineHeight: '0' }}>
+						{welcomeBoldCode}
+					</WelcomeTextStyle>
+				</WelcomeBoxStyle>
+
+				<EraserStyle>
+					<Eraser />
+				</EraserStyle>
+				<Pen2Style>
+					<Pen2 />
+				</Pen2Style>
+
+				<Pen3Style>
+					<Pen3 />
+				</Pen3Style>
+			</Container>
+		);
+	}
 };
 
 const WelcomeAnimated = () => (
