70d25d66686eed1dc32f151eea519630b9f692d5
xuezzou <ilfam1999@gmail.com>
2019-09-22 12:59:36 -0500

    try put import css after importing componnets

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 // try build again before i push it
 import styled from 'styled-components';
 
-import '../../node_modules/animate.css/animate.min.css';
-import '../css/layout.css';
-
 import BigLogoWithGridAnimated from '../components/bigLogo';
 import WelcomeAnimated from '../components/welcome';
 import FAQAnimated from '../components/faq/faq';
 import ScheduleAnimated from '../components/schedule';
 import SponsorBoxAnimated from '../components/sponsorBox';
+
+import '../../node_modules/animate.css/animate.min.css';
+import '../css/layout.css';
 
 const Container = styled.div`
 	width: 76%;
