5f637f13abec2891b73f814c4ffe309b567c116e
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2019-10-12 17:03:22 -0500

    Add MLH banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,29 @@ import HeaderInfo from '../components/modernism/headerInfo';
 
 const IndexPage = () => (
 	<div>
+		<a
+			id="mlh-trust-badge"
+			href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020_seaons&utm_content=blue"
+			target="_blank"
+			rel="noopener noreferrer"
+			style={{
+				display: 'block',
+				maxWidth: '100px',
+				minWidth: '60px',
+				position: 'fixed',
+				right: '50px',
+				top: '0px',
+				width: '10%',
+				zIndex: '10000',
+			}}
+		>
+			<img
+				src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-blue.svg"
+				alt="Major League Hacking 2020 Hackathon Season"
+				style={{ width: '100%' }}
+			/>
+		</a>
+
 		<SEO title="" />
 		{
 			// insert VH logo
