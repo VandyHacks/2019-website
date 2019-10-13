5fe475740f54137a8072f2ec6201db1f924c0f8c
Matt Leon <matthew.leon@vanderbilt.edu>
2019-10-12 19:07:25 -0500

    Add sponsor logos to website

--- src/components/sponsorBox.js
+++ src/components/sponsorBox.js
@@ -5,11 +5,21 @@ import InViewMonitor from 'react-inview-monitor';
 
 import SponsorTitle from '../images/sponsorText.svg';
 import isMobileContext from './isMobileContext';
-import UpperBorderBar from '../images/faqUpperBar.svg';
-import LowerBorderBar from '../images/faqLowerBar.svg';
 import Grid from '../images/gridLogo.svg';
-import Asurion from '../images/asurion.svg';
-import Fulcrum from '../images/fulcrumgt.svg';
+import Asurion from '../images/sponsors/headline/asurion.svg';
+import Fulcrum from '../images/sponsors/headline/fulcrumgt.svg';
+import AllianceBernstein from '../images/sponsors/platinum/alliance-bernstein.svg';
+import CapitalOne from '../images/sponsors/silver/capital-one.svg';
+import GIPHY from '../images/sponsors/silver/giphy.svg';
+import RedVentures from '../images/sponsors/silver/red-ventures.svg';
+import BNYMellon from '../images/sponsors/gold/bny_horizontal.svg';
+import L3Harris from '../images/sponsors/gold/l3-harris.svg';
+import Hillman from '../images/sponsors/bronze/hillman-black.svg';
+import Microsoft from '../images/sponsors/bronze/microsoft.svg';
+import IMCTrading from '../images/sponsors/bronze/imc-trading.svg';
+import Vanguard from '../images/sponsors/bronze/vanguard.svg';
+import StickerMule from '../images/sponsors/campus/stickermule.svg';
+import VUSE from '../images/sponsors/bronze/vuse.svg';
 
 const Container = styled.div`
 	width: 100%;
@@ -39,8 +49,9 @@ const SponsorContainer = styled.div`
 	justify-items: center;
 	align-items: center;
 	grid-gap: ${({ isMobile }) => (isMobile ? '10px' : '2em')};
-	margin-bottom: 3em;
+	margin-bottom: 1em;
 	font-size: 0.75em;
+	align-items: center;
 `;
 
 const LogoGridStyleMobile = styled.div`
@@ -54,9 +65,49 @@ const LogoGridStyleMobile = styled.div`
 	z-index: -1;
 `;
 
+const sponsors = {
+	headline: [
+		{ name: 'Asurion', link: 'https://www.asurion.com/', Logo: Asurion },
+		{ name: 'Fulcrum GT', link: 'https://fulcrumgt.com/jobs/', Logo: Fulcrum },
+	],
+	platinum: [
+		{
+			name: 'Alliance Bernstein',
+			link: 'https://www.alliancebernstein.com/',
+			Logo: AllianceBernstein,
+		},
+	],
+	gold: [
+		{ name: 'L3 Harris', link: 'https://www2.l3t.com/ForceX/', Logo: L3Harris },
+		{ name: 'BNY Mellon', link: 'https://www.bnymellon.com/', Logo: BNYMellon },
+	],
+	silver: [
+		{ name: 'Capital One', link: 'https://campus.capitalone.com/', Logo: CapitalOne },
+		{ name: 'Red Ventures', link: 'https://www.redventures.com/', Logo: RedVentures },
+		{
+			name: "Macy' Tech",
+			link: 'https://technology.macysjobs.com/',
+			Logo: () => <img style={{ margin: 0 }} src="/macys-tech.jpg" alt="Macy's Tech Logo" />,
+		},
+		{ name: 'GIPHY', link: 'https://giphy.com/', Logo: GIPHY },
+	],
+	bronze: [
+		{
+			name: 'Vanderbilt University School of Engineering',
+			link: 'https://engineering.vanderbilt.edu/',
+			Logo: VUSE,
+		},
+		{ name: 'IMC Trading', link: 'https://www.imc.com/us/', Logo: IMCTrading },
+		{ name: 'Microsoft', link: 'https://microsoft.com/', Logo: Microsoft },
+		{ name: 'Hillman', link: 'https://www.hillmangroup.com/us/en', Logo: Hillman },
+		{ name: 'Vanguard', link: 'https://investor.vanguard.com/corporate-portal/', Logo: Vanguard },
+	],
+	campus: [{ name: 'Sticker Mule', link: 'https://www.stickermule.com/', Logo: StickerMule }],
+};
+
 const Sponsors = ({ isMobile }) => (
-	<SponsorContainer isMobile>
-		<p
+	<SponsorContainer isMobile={isMobile}>
+		{/* <p
 			style={{
 				fontSize: '1.4em',
 				gridColumn: '1 / span 2',
@@ -68,35 +119,111 @@ const Sponsors = ({ isMobile }) => (
 			}}
 		>
 			Special thanks to our Headline sponsors:
-		</p>
-		<a
-			href="https://www.asurion.com/"
-			target="_blank"
-			rel="noopener noreferrer"
-			style={{ display: 'contents' }}
-		>
-			<Asurion style={{ gridColumn: '1 / span 2', maxHeight: '150px', pointerEvents: 'auto' }} />
-		</a>
-		<a
-			href="https://fulcrumgt.com/jobs/"
-			target="_blank"
-			rel="noopener noreferrer"
-			style={{ display: 'contents' }}
-		>
-			<Fulcrum style={{ gridColumn: '1 / span 2', maxHeight: '150px', pointerEvents: 'auto' }} />
-		</a>
-		<p
-			style={{
-				fontSize: '1.4em',
-				gridColumn: '1 / span 2',
-				margin: 0,
-				color: '#3048a1',
-				fontWeight: 'bold',
-				paddingLeft: isMobile ? 0 : '1.75em',
-			}}
-		>
-			Check back soon for our full sponsor list!
-		</p>
+		</p> */}
+		{sponsors.headline.map(sponsor => (
+			<a
+				href={sponsor.link}
+				title={sponsor.name}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: 'contents' }}
+				key={sponsor.link}
+			>
+				<sponsor.Logo
+					style={{ gridColumn: '1 / span 2', maxHeight: '150px', pointerEvents: 'auto' }}
+				/>
+			</a>
+		))}
+		{sponsors.platinum.map(sponsor => (
+			<a
+				href={sponsor.link}
+				title={sponsor.name}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: 'contents' }}
+				key={sponsor.link}
+			>
+				<sponsor.Logo
+					style={{
+						gridColumn: '1 / span 2',
+						maxHeight: '125px',
+						pointerEvents: 'auto',
+						maxWidth: '100%',
+					}}
+				/>
+			</a>
+		))}
+		{sponsors.gold.map(sponsor => (
+			<a
+				href={sponsor.link}
+				title={sponsor.name}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: 'contents' }}
+				key={sponsor.link}
+			>
+				<sponsor.Logo
+					style={{
+						gridColumn: '1 / span 2',
+						maxHeight: '100px',
+						pointerEvents: 'auto',
+						width: '75%',
+					}}
+				/>
+			</a>
+		))}
+		{sponsors.silver.map(sponsor => (
+			<a
+				href={sponsor.link}
+				title={sponsor.name}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: 'contents' }}
+				key={sponsor.link}
+			>
+				<sponsor.Logo
+					style={{
+						gridColumn: 'auto',
+						maxHeight: '100px',
+						pointerEvents: 'auto',
+						width: '100%',
+					}}
+				/>
+			</a>
+		))}
+		{sponsors.bronze.map(sponsor => (
+			<a
+				href={sponsor.link}
+				title={sponsor.name}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: 'contents' }}
+				key={sponsor.link}
+			>
+				<sponsor.Logo
+					style={{
+						gridColumn: 'auto',
+						maxHeight: '75px',
+						pointerEvents: 'auto',
+						width: '75%',
+					}}
+				/>
+			</a>
+		))}
+		{sponsors.campus.map(sponsor => (
+			<a
+				href={sponsor.link}
+				title={sponsor.name}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: 'contents' }}
+				key={sponsor.link}
+			>
+				<sponsor.Logo
+					style={{ gridColumn: 'auto', maxHeight: '60px', width: '60%', pointerEvents: 'auto' }}
+				/>
+			</a>
+		))}
 	</SponsorContainer>
 );
 
